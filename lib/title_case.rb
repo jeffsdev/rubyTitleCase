abfee3810dc371d977e1956f4a5b9fb0799b836e
Jeff Seymour, Kyle Langley and Krystan Menne <kyle@>
2016-01-04 13:28:07 -0800

    capitalizes first letter of multiple word string and not designated words.

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,5 +1,40 @@
+# Behavior: It capitalizes the first letter of a single word
+# Input: beowulf
+# Output: Beowulf
+
+# Behavior: It capitalizes the first letter of a multiple word string
+# Input: beowulf begins
+# Output: Beowulf Begins
+
+# Behavior: It does not capitalize designated words (prepositions, conjunctions, etc)
+# Input: beowulf from brighton beach
+# Output: Beowulf from Brighton Beach
+
+# Behavior: It capitalizes designated words if they are the first word
+# Input: from beowulf to the hulk
+# Output: From Beowulf to the Hulk
+
+# Behavior: It handles non-letter characters
+# Input: 57 beowulf alternate endings!!
+# Output: 57 Beowulf Alternate Endings!!
+
+# Behavior: It manages all uppercase entries
+# Input: BEOWULF ON THE ROCKS
+# Output: Beowulf on the Rocks
+
+# Behavior: It manages mixed case entries
+# Input: BeoWulf aNd mE
+# Output: Beowulf and Me
+
 class String
   define_method(:title_case) do
-    self.capitalize!()
+    non_cap_words = ["the", "and", "of", "or", "from", "but", "to", "on"]
+    split_sentence = self.split()
+    split_sentence.each() do |word|
+      if !non_cap_words.include?(word)
+        word.capitalize!()
+      end
+    end
+    split_sentence.join(" ")
   end
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,10 @@ describe('String#title_case') do
   it("capitalizes the first letter of a word") do
     expect(("beowolf").title_case()).to(eq("Beowolf"))
   end
+  it("capitalizes the first letter of a multiple word string") do
+    expect(("beowulf begins").title_case()).to(eq("Beowulf Begins"))
+  end
+  it("does not capitalize designated words (prepositions, conjunctions, etc)") do
+    expect(("beowulf from brighton beach").title_case()).to(eq("Beowulf from Brighton Beach"))
+  end
 end
