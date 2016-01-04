9f2a6210324c1d186fcd437ca7bab771c468be3f
Jeff Seymour, Kyle Langley and Krystan Menne <jeff@>
2016-01-04 13:45:12 -0800

    manages mixed case entries

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -29,7 +29,7 @@
 class String
   define_method(:title_case) do
     non_cap_words = ["the", "and", "of", "or", "from", "but", "to", "on"]
-    split_sentence = self.split()
+    split_sentence = self.downcase().split()
     split_sentence.each() do |word|
       if !non_cap_words.include?(word) || split_sentence.index(word) == 0
         word.capitalize!()
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -14,4 +14,13 @@ describe('String#title_case') do
   it("capitalizes designated words if they are the first word") do
     expect(("from beowulf to the hulk").title_case()).to(eq("From Beowulf to the Hulk"))
   end
+  it("handles non-letter characters") do
+    expect(("57 beowulf alternate endings!!").title_case()).to(eq("57 Beowulf Alternate Endings!!"))
+  end
+  it("manages all uppercase entries") do
+    expect(("BEOWULF ON THE ROCKS").title_case()).to(eq("Beowulf on the Rocks"))
+  end
+  it("manages mixed case entries") do
+    expect(("BeoWulf aNd mE").title_case()).to(eq("Beowulf and Me"))
+  end
 end
